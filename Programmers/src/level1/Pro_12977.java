58570144bada43eb4d25dbc0b12c3ab29282cf50
UCDAyoung <qkr93643857@gmail.com>
2021-07-14 00:33:59 +0900

    swift추가

---
+++ Programmers/src/level1/Pro_12977.java
@@ -0,0 +1,15 @@
+/* 작성 : 21.06.23
+ * 소수만들기 
+ * 
+ * 
+ */
+package level1;
+
+public class Pro_12977 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Swift/BOJ_2557.playground/Contents.swift
+++
@@ -1,7 +0,0 @@
-/* 오늘부로 Swift 시작
- Hello Swift!
- */
-
-var greeting = "Hello World!"
-print(greeting)
-
